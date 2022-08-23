af9ed9f10a45fc9035caffedeb1832ccd74c858b
MansoorAbbasi <mansoorfaizy9541@gmail.com>
2022-08-24 02:42:16 +0300

    A.2

---
+++ src/Day2.java
@@ -0,0 +1,14 @@
+public class Day2 {
+    /*
+          eger bir elementle ile ilgili tutacagimiz bilgiler coksa veya kompleks ise
+          bu defa ic ice map kullanmayi tercih edebiliriz
+          ic ice map kullandigimizda daha fazla datayi,
+          daha duzenli olarak tutma imkanimiz olur
+          Ancak, bilgiye ulasmak ve manipule etmek zorlasacaktir.
+          Ornek olarak dunku map'imizi map'lerden olusan bir map olarak olusturalim
+          sinifMap.put(no=102,isim=Enes,soyisim= Cem,brans= Tester);
+          sinifMap.put(103,"Taha, Emre, JDev");
+          sinifMap.put(104,"Derya, Deniz, Devops");
+         */
+}
+
