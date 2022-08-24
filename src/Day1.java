9df3a001fe485551c0952f1d9abd39f71e9a5a9e
MansoorAbbasi1 <108625629+MansoorAbbasi1@users.noreply.github.com>
2022-08-24 15:59:52 +0300

    Update Day1.java

--- src/Day1.java
+++ src/Day1.java
@@ -5,5 +5,6 @@ public class Day1 {  public static void main(String[] args) {
     System.out.println("Git 1. Deneme");
     System.out.println("Git 2. Deneme");
     System.out.println("Git 3. Deneme");
+     System.out.println("Git 4. Deneme");
 }
 }
